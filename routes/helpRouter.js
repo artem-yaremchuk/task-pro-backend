796aa84b13875fcc046d30a7e54267fc3dbc0600
Bohdan Ses <91324589+boderses@users.noreply.github.com>
2024-03-03 21:28:42 +0100

    feat: add nodemailer and possibility of sending email to support

--- routes/helpRouter.js
+++ routes/helpRouter.js
@@ -1,7 +1,8 @@
 import express from "express";
+import { emailSupport } from "../controllers/authControllers.js";
 
 const helpRouter = express.Router();
 
-helpRouter.post("/" );
+helpRouter.post("/", emailSupport);
 
 export default helpRouter;

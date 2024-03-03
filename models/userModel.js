bca685be4e2856f7afd77571eadf73540b922279
artem-yaremchuk <artem.yaremchuk@yahoo.com>
2024-03-03 14:43:42 +0200

    Feat: Comment user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,45 +1,45 @@
-import { model, Schema } from "mongoose";
-import { compare, genSalt, hash } from "bcrypt";
-import {
-  themeTypes
-} from "../constants.js";
+// import { model, Schema } from "mongoose";
+// import { compare, genSalt, hash } from "bcrypt";
+// import {
+//   themeTypes
+// } from "../constants.js";
 
-const userSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: [true, "Username is required"],
-    },
-    email: {
-      type: String,
-      required: [true, "Email is required"],
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: [true, "Set password for user"],
-    },
-    theme: {
-      type: String,
-      enum: themeTypes,
-      default: "dark",
-    },
-    token: String,
-    avatarURL: String,
-  },
-  { versionKey: false, timestamps: true },
-);
+// const userSchema = new Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: [true, "Username is required"],
+//     },
+//     email: {
+//       type: String,
+//       required: [true, "Email is required"],
+//       unique: true,
+//     },
+//     password: {
+//       type: String,
+//       required: [true, "Set password for user"],
+//     },
+//     theme: {
+//       type: String,
+//       enum: themeTypes,
+//       default: "dark",
+//     },
+//     token: String,
+//     avatarURL: String,
+//   },
+//   { versionKey: false, timestamps: true },
+// );
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+// userSchema.pre("save", async function (next) {
+//   if (!this.isModified("password")) return next();
 
-  const salt = await genSalt(10);
-  this.password = await hash(this.password, salt);
+//   const salt = await genSalt(10);
+//   this.password = await hash(this.password, salt);
 
-  next();
-});
+//   next();
+// });
 
-userSchema.methods.checkPassword = (candidate, passwordHash) =>
-  compare(candidate, passwordHash);
+// userSchema.methods.checkPassword = (candidate, passwordHash) =>
+//   compare(candidate, passwordHash);
 
-export const User = model("User", userSchema);
+// export const User = model("User", userSchema);

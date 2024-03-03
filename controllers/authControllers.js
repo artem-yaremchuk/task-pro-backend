2087504559f4e2f02f9544d05849330146c7c7f7
Yara1504 <baykenichs@gmail.com>
2024-03-04 01:11:49 +0200

    auth + cloudinary

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -2,36 +2,63 @@ import catchAsync from "../helpers/catchAsync.js";
 import { signup, login } from "../services/usersServices.js";
 import { User } from "../models/userModel.js";
 
-export const registerUser = catchAsync(async (req, res) => {
-  const { email } = await signup(req.body);
+export const registerUser = async (req, res, next) => {
+    try {
+        const userData = req.body; 
+        const { token, user } = await signup(userData);
 
-  res.status(201).json({
-    user: {
-      email,
-      subscription: "starter",
-    },
-  });
-});
-
-export const loginUser = catchAsync(async (req, res) => {
-  const { email, token } = await login(req.body);
-
-  res.status(200).json({
-    token,
-    user: {
-      email,
-      subscription: "starter",
-    },
-  });
-});
-
-export const getCurrentUser = (req, res) => {
-  const { email, subscription } = req.user;
-  res.status(200).json({
-    email,
-    subscription,
-  });
+      res.status(201).json({
+            token,
+            user: {
+                name: user.name,
+                email: user.email,
+                avatarURL: user.avatarURL,
+                boards: user.boards,
+                theme: user.theme,
+            }
+      });
+    } catch (error) {
+        next(error);
+    }
 };
+
+export const loginUser = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const user = await login(email, password);
+        res.status(201).json({ user });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getCurrentUser = async (req, res, next) => {
+    try {
+        const { _id, name, email, avatarURL, boards, theme } = req.user;
+        
+        await User.findById(_id, { new: true })
+            .populate("boards", {
+                _id: 1,
+                title: 1,
+                icon: 1,
+                background: 1,
+                updatedAt: 1,
+            })
+            .then((user) => {
+                res.status(201).json({
+                    user: {
+                    name,
+                    email,
+                    avatarURL,
+                    boards,
+                    theme,                   
+                }
+            })            
+        })
+    } catch (error) {
+        next(error)
+    }
+}
 
 export const logoutUser = catchAsync(async (req, res) => {
   const { _id } = req.user;
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,51 +1,25 @@
 import HttpError from "../helpers/HttpError.js";
-import catchAsync from "../helpers/catchAsync.js";
 import { User } from "../models/userModel.js";
-import {
-  registerDataValidator,
-  loginDataValidator,
-} from "../helpers/userValidators.js";
-import { checkToken } from "../services/jwtService.js";
+import jwt from "jsonwebtoken";
 
-export const checkRegisterData = catchAsync(async (req, res, next) => {
-  const { value, error } = registerDataValidator(req.body);
+const { JWT_SECRET } = process.env;
 
-  if (error) throw HttpError(400, error.message);
-
-  const user = await User.findOne({ email: value.email });
-
-  if (user) {
-    throw HttpError(409, "Email in use");
+export const authorization = async (req, res, next) => {
+  const { authorization = "" } = req.headers;
+  const [bearer, token] = authorization.split(" ");
+  if (bearer !== "Bearer") {
+    next(HttpError(401));
   }
-
-  req.body = value;
-
-  next();
-});
-
-export const checkLoginData = (req, res, next) => {
-  const { value, error } = loginDataValidator(req.body);
-
-  if (error) throw HttpError(400, error.message);
-
-  req.body = value;
-
-  next();
+  try {
+    const { id } = jwt.verify(token, JWT_SECRET);
+    const user = await User.findById(id);
+    if (!user || !user.token || user.token !== token) {
+      next(HttpError(401));
+    }
+    req.user = user;
+    next();
+  } catch {
+    next(HttpError(401));
+  }
 };
 
-export const protect = catchAsync(async (req, res, next) => {
-  const token =
-    req.headers.authorization?.startsWith("Bearer ") &&
-    req.headers.authorization.split(" ")[1];
-  const userId = checkToken(token);
-
-  if (!userId) throw HttpError(401, "Not authorized");
-
-  const currentUser = await User.findById(userId);
-
-  if (!currentUser) throw HttpError(401, "Not authorized");
-
-  req.user = currentUser;
-
-  next();
-});

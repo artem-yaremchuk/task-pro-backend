f18227a614c8bef727293cef64b6c2a149389bb7
Bohdan Ses <91324589+boderses@users.noreply.github.com>
2024-03-05 18:21:59 +0100

    feat:add_possibility_to_update_theme_for_user

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -4,62 +4,62 @@ import { User } from "../models/userModel.js";
 import sendEmail from "../helpers/sendEmail.js";
 
 export const registerUser = catchAsync(async (req, res, next) => {
-    try {
-        const userData = req.body; 
-        const { token, user } = await signup(userData);
+  try {
+    const userData = req.body;
+    const { token, user } = await signup(userData);
 
-      res.status(201).json({
-            token,
-            user: {
-                name: user.name,
-                email: user.email,
-                avatarURL: user.avatarURL,
-                boards: user.boards,
-                theme: user.theme,
-            }
-      });
-    } catch (error) {
-        next(error);
-    }
+    res.status(201).json({
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+        avatarURL: user.avatarURL,
+        boards: user.boards,
+        theme: user.theme,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 export const loginUser = catchAsync(async (req, res, next) => {
-    try {
-        const { email, password } = req.body;
-        const user = await login(email, password);
-        res.status(201).json({ user });
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const { email, password } = req.body;
+    const user = await login(email, password);
+    res.status(201).json({ user });
+  } catch (error) {
+    next(error);
+  }
 });
 
 export const getCurrentUser = async (req, res, next) => {
-    try {
-        const { _id, name, email, avatarURL, boards, theme } = req.user;
-        
-        await User.findById(_id, { new: true })
-            .populate("boards", {
-                _id: 1,
-                title: 1,
-                icon: 1,
-                background: 1,
-                updatedAt: 1,
-            })
-            .then((user) => {
-                res.status(201).json({
-                    user: {
-                    name,
-                    email,
-                    avatarURL,
-                    boards,
-                    theme,                   
-                }
-            })            
-        })
-    } catch (error) {
-        next(error)
-    }
-}
+  try {
+    const { _id, name, email, avatarURL, boards, theme } = req.user;
+
+    await User.findById(_id, { new: true })
+      .populate("boards", {
+        _id: 1,
+        title: 1,
+        icon: 1,
+        background: 1,
+        updatedAt: 1,
+      })
+      .then((user) => {
+        res.status(201).json({
+          user: {
+            name,
+            email,
+            avatarURL,
+            boards,
+            theme,
+          },
+        });
+      });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const logoutUser = catchAsync(async (req, res) => {
   const { _id } = req.user;
@@ -69,7 +69,7 @@ export const logoutUser = catchAsync(async (req, res) => {
   res.status(204).send();
 });
 
-export const emailSupport = async (req, res) => {
+export const emailSupport = catchAsync(async (req, res) => {
   const { email, comment } = req.body;
 
   const helpRequest = {
@@ -88,18 +88,19 @@ export const emailSupport = async (req, res) => {
   res.json({
     message: "Reply email has been sent",
   });
-};
+});
 
-export const updateTheme = async (req, res) => {
+export const updateTheme = catchAsync(async (req, res) => {
+  try {
     const { _id } = req.user;
     const { theme } = req.body;
 
-    const result = await User.findByIdAndUpdate(_id, req.body);
-    if (!result) throw HttpError(404);
-  
+    await User.findByIdAndUpdate(_id, req.body);
     res.json({
       theme,
       message: `The theme has been changed.`,
     });
-  };
-
+  } catch (error) {
+    next(error);
+  }
+});
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,15 +1,18 @@
 import express from "express";
 // import { registerUser, loginUser, getCurrentUser, logoutUser, updateTheme, updateUserProfile } from "../controllers/authControllers.js";
 // import { checkRegisterData, checkLoginData, protect, checkThemeData, uploadAvatar, checkProfileData } from "../middlewares/authMiddleware.js";
-import { registerUser, loginUser, getCurrentUser, logoutUser} from "../controllers/authControllers.js";
+import { registerUser, loginUser, getCurrentUser, logoutUser, updateTheme} from "../controllers/authControllers.js";
 import { authorization } from "../middlewares/authMiddleware.js";
+import { validateBody } from "../middlewares/validateBody.js";
+import { themeSchema } from "../schemas/usersSchemas.js";
+
 const authRouter = express.Router();
 
 authRouter.post("/register", registerUser);
 authRouter.post("/login", loginUser);
 authRouter.get("/current", authorization, getCurrentUser);
 authRouter.post("/logout", authorization, logoutUser);
-// authRouter.patch("/themes", protect, checkThemeData, updateTheme); // to do
+authRouter.patch("/themes", authorization, validateBody(themeSchema), updateTheme);
 // authRouter.patch("/update", protect, uploadAvatar, checkProfileData, updateUserProfile); // to do
 
 export default authRouter;

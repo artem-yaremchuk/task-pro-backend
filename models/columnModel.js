03ba746f2be8ffbf275286e32d3a4ccd9b5ee3e0
Vladimir Galaiko <a100war@gmail.com>
2024-03-06 01:21:55 +0200

    add updateColumn
    updateColumn

--- models/columnModel.js
+++ models/columnModel.js
@@ -1,32 +1,29 @@
-import { model, Schema } from "mongoose";
-
-
+import { model, Schema } from 'mongoose'
 
 const columnSchema = new Schema(
-    {
-      title: {
-        type: String,
-        required: [true, "Set title for column"],
-      },
-      user: {
-        type: Schema.Types.ObjectId,
-        ref: "user",
-        required: true,
-      },
-      board: {
-        type: Schema.Types.ObjectId,
-        ref: "board",
-        required: true,
-      },
-      tasks: [
-        {
-          type: Schema.Types.ObjectId,
-          ref: "task",
-        },
-      ],
+  {
+    title: {
+      type: String,
+      required: [true, 'Set title for column'],
     },
-    { versionKey: false, timestamps: true }
-  );
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+    board: {
+      type: Schema.Types.ObjectId,
+      ref: 'board',
+      required: true,
+    },
+    tasks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'task',
+      },
+    ],
+  },
+  { versionKey: false, timestamps: true }
+)
 
-
-  export const Column = model("column", columnSchema);
+export const Column = model('column', columnSchema)

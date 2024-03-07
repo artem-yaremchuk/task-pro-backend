d35b32c1f6d094c9fe09d4f83fdc45c321b0e86b
artem-yaremchuk <artem.yaremchuk@yahoo.com>
2024-03-07 17:08:07 +0200

    Fix: Delete userValidators.js

--- helpers/userValidators.js
+++
@@ -1,5 +0,0 @@
-import { registerUserSchema, loginUserSchema } from "../schemas/usersSchemas.js";
-
-export const registerDataValidator = (data) => registerUserSchema.validate(data);
-
-export const loginDataValidator = (data) => loginUserSchema.validate(data);

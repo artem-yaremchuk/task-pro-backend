1f0b8bf182bc9290406f825d06d7f0278c479027
artem-yaremchuk <artem.yaremchuk@yahoo.com>
2024-03-10 14:15:02 +0200

    Fix: Add upload import into authRouter

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,18 +1,33 @@
 import express from "express";
-// import { registerUser, loginUser, getCurrentUser, logoutUser, updateTheme, updateUserProfile } from "../controllers/authControllers.js";
-// import { checkRegisterData, checkLoginData, protect, checkThemeData, uploadAvatar, checkProfileData } from "../middlewares/authMiddleware.js";
-import { registerUser, loginUser, getCurrentUser, logoutUser, updateTheme, updateUser} from "../controllers/authControllers.js";
+import {
+  registerUser,
+  loginUser,
+  getCurrentUser,
+  logoutUser,
+  updateTheme,
+  updateUser,
+} from "../controllers/authControllers.js";
 import { authorization } from "../middlewares/authMiddleware.js";
 import { validateBody } from "../middlewares/validateBody.js";
-import { themeSchema, registerUserSchema, loginUserSchema } from "../schemas/usersSchemas.js";
+import {
+  themeSchema,
+  registerUserSchema,
+  loginUserSchema,
+} from "../schemas/usersSchemas.js";
+import upload from "../middlewares/upload.js";
 
 const authRouter = express.Router();
 
-authRouter.post("/register", validateBody(registerUserSchema) ,registerUser);
-authRouter.post("/login", validateBody(loginUserSchema) ,loginUser);
+authRouter.post("/register", validateBody(registerUserSchema), registerUser);
+authRouter.post("/login", validateBody(loginUserSchema), loginUser);
 authRouter.get("/current", authorization, getCurrentUser);
 authRouter.post("/logout", authorization, logoutUser);
-authRouter.patch("/themes", authorization, validateBody(themeSchema), updateTheme);
-authRouter.patch("/update", authorization , upload.single("avatar"), updateUser)
+authRouter.patch(
+  "/themes",
+  authorization,
+  validateBody(themeSchema),
+  updateTheme,
+);
+authRouter.patch("/update", authorization, upload.single("avatar"), updateUser);
 
 export default authRouter;
